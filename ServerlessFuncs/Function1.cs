c9fbc6bb688f5473a6f7de2499c9fc4b77f17406
nirzaf <mfmfazrin1986@gmail.com>
2021-09-08 22:34:21 +0530

    commit

--- ServerlessFuncs/Function1.cs
+++ ServerlessFuncs/Function1.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
 using System.Collections.Generic;
